ec6c44e13a85dc20e75099dd5f4827fc1b8cd983
Tung Tran <ttran1185@gmail.com>
2020-10-26 09:51:56 -0700

    update

---
+++ public/service-worker.js
@@ -0,0 +1,70 @@
+var FILES_TO_CACHE = [
+    '/',
+    '/styles.css',
+    '/index.js',
+    '/index.html',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/manifest.json',
+    '/db.js',
+    './favicon.ico'
+    
+  
+  ];
+  
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  self.addEventListener("install", (evt) => {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    self.skipWaiting();
+  });
+
+  self.addEventListener("activate", (evt) => {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+    self.clients.claim();
+  });
+
+  self.addEventListener("fetch", (evt) => {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then(cache => {
+            return fetch(evt.request)
+              .then((response) => {
+                if (response.status === 200) {
+                  cache.put(evt.request, response.clone());
+                }
+                return response;
+              })
+              .catch(() => {
+                return cache.match(evt.request);
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+  
+      return;
+    }
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      })
+    );
+  });
+  
